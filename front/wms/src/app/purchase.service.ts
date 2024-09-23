945ceb41482a4b5fb80c1c11842bc4b60b0e74fb
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-23 18:47:04 +0530

    added api integration for purchase

--- front/wms/src/app/purchase.service.ts
+++ front/wms/src/app/purchase.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const baseurl = 'http://localhost:8080';
 @Injectable({
   providedIn: 'root'
 })
+
 export class PurchaseService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
+  public getAll(){
+    return this.http.get(baseurl+'/purchase')
+  }
+
+  get(id:number){
+    return this.http.get(`${baseurl}/purchase/${id}`)
+  }
+
+  approve(id:number){
+    return this.http.post(`${baseurl}/purchase/front/approve`,{ref:id})
+  }
 
 }
