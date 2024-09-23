bcd32b253803b096444f7bff8777ce9fac45bfaa
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-23 16:27:17 +0530

    added purchase component and purchase service

--- front/wms/src/app/app.module.ts
+++ front/wms/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { PurchaseService } from './purchase.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PurchaseComponent } from './purchase/purchase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PurchaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PurchaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
